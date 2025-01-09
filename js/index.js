bc5b1dbff52c3d7caf2e147f98b8f44894615ec8
Your Name <your.email@example.com>
2025-01-09 20:29:33 +0600

    element by id

---
+++ js/index.js
@@ -0,0 +1,4 @@
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+const mealResults = document.getElementById('meal-results');
+const showAllBtn = document.getElementById('show-all-btn');
